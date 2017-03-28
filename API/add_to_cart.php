27238005e039e3e23a30212b07ed2a33f7ee1f5d
kaisasvensson <kaisa.svensson@hotmail.com>
2017-03-28 23:34:34 +0200

    replaced array_push with []

--- API/add_to_cart.php
+++ API/add_to_cart.php
@@ -8,7 +8,7 @@ if (isset ($_GET['id'])) {
     $stm_select->execute();
     foreach ($stm_select as $row) {
         if (!empty($_SESSION['cart'])) if (is_array($_SESSION['cart'])) {
-            array_push($_SESSION['cart'], ["item" => $row, "quantity" => 1]); //Om det redan finns varor i varukorgen så lägger man till den nya produkten i arrayen
+            ($_SESSION['cart'][] = ["item" => $row, "quantity" => 1]); //Om det redan finns varor i varukorgen så lägger man till den nya produkten i arrayen
         } else {
             $_SESSION['cart'] = array(["item" => $row, "quantity" => 1]); //Om varukorgen är tom så skapas en array
         } else
