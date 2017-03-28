39c2879a5f4027971db763f02d4675c58066f349
kaisasvensson <kaisa.svensson@hotmail.com>
2017-03-28 23:00:57 +0200

    reformatted code in my_page_user_info

--- API/my_page_user_info.php
+++ API/my_page_user_info.php
@@ -1,20 +1,15 @@
 <?php
-
-include("auth.php"); //include auth.php file on all secure pages
-require('db.php');
+require 'config.php';
+require 'auth.php';
+header('Content-Type: application/json');
 
 $email = $_SESSION['email'];
 
-//Visar användarinformation på mina sidor
 $stm_select = $pdo->prepare('SELECT * FROM `users` WHERE email = :email');
 $stm_select->execute(['email' => $email]);
 $resultat = array();
 
 foreach($stm_select as $row) {
     $resultat[] = $row;
-
 }
-
-//JSON_UNESCAPED_UNICODE används för att kunna skriva ut åäö.
-echo json_encode($resultat, JSON_UNESCAPED_UNICODE);
-?>
+echo json_encode($resultat);
