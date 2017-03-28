222af7dd5e8d6775ed118ca6d51c9ab3c915d96e
kaisasvensson <kaisa.svensson@hotmail.com>
2017-03-28 23:05:25 +0200

    reformatted code and removed end tag in auth.php

--- API/auth.php
+++ API/auth.php
@@ -1,7 +1,6 @@
 <?php
-
 session_start();
-if(!isset($_SESSION["email"])){
-header("Location: login.php");  //Om användaren inte är inloggad skickas den vidare till inloggningssidan.
-exit(); }
-?>
+if (!isset($_SESSION["email"])) {
+    header("Location: login.php");  //Om användaren inte är inloggad skickas den vidare till inloggningssidan.
+    exit();
+}
