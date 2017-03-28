cd8552e0d804ed677103815127f063c75bf3d60d
kaisasvensson <kaisa.svensson@hotmail.com>
2017-03-28 22:46:55 +0200

    formatted code in my_page_order

--- API/my_page_order.php
+++ API/my_page_order.php
@@ -1,20 +1,12 @@
-<?php
-
-include("auth.php"); //include auth.php file on all secure pages
-require('db.php');
-
+require 'auth.php';
+require 'db.php';
 $email = $_SESSION['email'];
 
-//Visar användarens tidigare ordrar på mina sidor
-$stm_select = $pdo->prepare('SELECT * FROM `order` WHERE email = :email'); //Väljer den e-postadress som är inloggad
+$stm_select = $pdo->prepare('SELECT * FROM `order` WHERE email = :email'); 
 $stm_select->execute(['email' => $email]);
 $resultat = array();
 
-foreach($stm_select as $row) {
+foreach ($stm_select as $row) {
     $resultat[] = $row;
-
 }
-
-//JSON_UNESCAPED_UNICODE används för att kunna skriva ut åäö.
-echo json_encode($resultat, JSON_UNESCAPED_UNICODE);
-?>
+echo json_encode($result);
