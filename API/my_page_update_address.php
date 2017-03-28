db72251e408b54a50c1d9d2ec1736d323e2782b5
kaisasvensson <kaisa.svensson@hotmail.com>
2017-03-28 22:57:10 +0200

    reformatted code in my_page_update_adress

--- API/my_page_update_address.php
+++ API/my_page_update_address.php
@@ -1,15 +1,13 @@
 <?php
+require 'config.php';
+require 'auth.php';
+header('Content-Type: application/json');
 
-// Uppdaterar användarens addressuppgifter (address, postnummer och postort) genom ett formulär på mina sidor
-require('db.php');
-include("auth.php");
 $email = $_SESSION['email'];
 
-if( isset($_POST['faddress']) ) {
+if (isset($_POST['faddress'])) {
     $sql = "UPDATE `users` SET `address` = :vaddress, `zipcode` = :vzipcode, `city` = :vcity WHERE `email` = :vemail";
     $stm_update = $pdo->prepare($sql);
     $stm_update->execute(['vaddress' => $_POST['faddress'], 'vzipcode' => $_POST['fzipcode'], 'vcity' => $_POST['fcity'], 'vemail' => $email]);
     echo json_encode(true);
-    }
-
-?>
+}
