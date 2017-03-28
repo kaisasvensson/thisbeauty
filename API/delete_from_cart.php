b103de513c9a97cebfbef9a8afd19339a921f545
kaisasvensson <kaisa.svensson@hotmail.com>
2017-03-28 22:28:17 +0200

    formatted code in delete_from_cart

--- API/delete_from_cart.php
+++ API/delete_from_cart.php
@@ -2,12 +2,7 @@
 session_start();
 
 if (isset ($_GET['id'])) {
-require 'db.php';
-
-echo json_encode(array_splice($_SESSION['cart'], 0, 1));
-    
-    //array_splice = Remove elements from an array and replace it with new elements
-
-    
+    require 'db.php';
+    echo json_encode(array_splice($_SESSION['cart'], 0, 1));
 }
-?>
+
